719e907df0bb1ba88ae4bf00b2626fc6437fafa3
ramji.omar <ramji.omar@ibaset.com>
2020-05-25 15:21:00 +0530

    build

---
+++ build/precache-manifest.5bf0d06ff7ba371f169bd2b96fd02e3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f94e567f8e242ea28b85b064bbefe90",
+    "url": "/letusscrum/index.html"
+  },
+  {
+    "revision": "bf195757c005cc9289da",
+    "url": "/letusscrum/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "160bc0847af2f5080c54",
+    "url": "/letusscrum/static/js/2.7d8ad548.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/letusscrum/static/js/2.7d8ad548.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf195757c005cc9289da",
+    "url": "/letusscrum/static/js/main.f4f05c94.chunk.js"
+  },
+  {
+    "revision": "00c644e3728163af4498",
+    "url": "/letusscrum/static/js/runtime-main.56a95bda.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/letusscrum/static/media/logo.5d5d9eef.svg"
+  }
+]);
